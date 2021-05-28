d88d831c2271f033b94a04cca01bcb5f8ce8a1b9
Joel Beckum <joelbeckum@gmail.com>
2021-05-28 14:28:52 -0400

    Initial commit and first array creation assignment for project

---
+++ shopping.js
@@ -0,0 +1,24 @@
+const shoppingList = [
+    {
+        id: 1,
+        name: "milk",
+        price: 3
+    },
+    {
+        id: 2,
+        name: "bread",
+        price: 3.50
+    },
+    {
+        id: 3,
+        name: "eggs",
+        price: 2
+    },
+    {
+        id: 4,
+        name: "bananas",
+        price: 1.50
+    }
+]
+
+console.log(shoppingList)
